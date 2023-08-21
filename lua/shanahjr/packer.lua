17e4577fc4f7fd4f194759bb60a5a0a8d2a9dda9
Shanah Datacloud <shanah@datacloud.co.bw>
2023-08-21 11:57:18 +0200

    Configuration Updates
    1. Added Keybindings to fugitive.lua to handle git status and diff
    2. Added packages to packer
    3. Added remap for ctrl d and u

--- lua/shanahjr/packer.lua
+++ lua/shanahjr/packer.lua
@@ -22,6 +22,7 @@ use ('nvim-treesitter/playground')
 use ('theprimeagen/harpoon')
 use ('mbbill/undotree')
 use ('tpope/vim-fugitive')
+use ('preservim/nerdcommenter')
 use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
